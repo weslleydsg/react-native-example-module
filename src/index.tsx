578041916aa7132fa57c79611a1a25e60221c796
Weslley da Silva Gomes <weslleydsg21@gmail.com>
2023-01-21 21:07:19 -0300

    feat: create subtract operator

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
-
 export function add(a: number, b: number): Promise<number> {
   return Promise.resolve(a + b);
+}
+
+export function subtract(a: number, b: number): Promise<number> {
+  return Promise.resolve(a + b);
+}
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Promise.resolve(a * b);
 }
