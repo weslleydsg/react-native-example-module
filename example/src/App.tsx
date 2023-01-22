8df0d48b2a6584f560f05c9ae4f493f42eea5b3d
Weslley da Silva Gomes <weslleydsg21@gmail.com>
2023-01-21 21:41:22 -0300

    revert: sync operator back to async operator
    This reverts commit 7adcc225f4d596f9ae0b7c83d2b82b8b103aace3.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    setResult(multiply(3, 7));
+    multiply(3, 7).then(setResult);
   }, []);
 
   return (
