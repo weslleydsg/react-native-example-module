b4fef81cdf136ebe318922129fa890a1e1560d52
Weslley da Silva Gomes <weslleydsg21@gmail.com>
2023-01-25 22:16:56 -0300

    feat: create subtract operator

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,18 +5,23 @@ import { add, multiply } from 'react-native-example-package';
 
 export default function App() {
   const [addResult, setAddResult] = React.useState<number | undefined>();
+  const [subtractResult, setSubtractResult] = React.useState<
+    number | undefined
+  >();
   const [multiplyResult, setMultiplyResult] = React.useState<
     number | undefined
   >();
 
   React.useEffect(() => {
     add(3, 7).then(setAddResult);
+    add(3, 7).then(setSubtractResult);
     multiply(3, 7).then(setMultiplyResult);
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Add Result: {addResult}</Text>
+      <Text>Subtract Result: {subtractResult}</Text>
       <Text>Multiply Result: {multiplyResult}</Text>
     </View>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ export function add(a: number, b: number): Promise<number> {
   return Promise.resolve(a + b);
 }
 
+export function subtract(a: number, b: number): Promise<number> {
+  return Promise.resolve(a + b);
+}
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
