1157741a7fe5716792dc4bbe57efd0b7aae7d4a2
Weslley da Silva Gomes <weslleydsg21@gmail.com>
2023-02-05 18:10:00 -0300

    feat: receive unlimited args from operators (#9)
    * ci: add beta branch on ci workflow
    
    * feat: receive unlimited args from operators
    
    * docs: update readme file
    
    BREAKING CHANGE: receive unlimited args from operators instead of object.

---
+++ src/utils/operators/index.ts
@@ -0,0 +1,19 @@
+type Args = [number, number, ...number[]];
+
+export function add(...args: Args): number {
+  return args.reduce((previousValue, value) => {
+    return previousValue + value;
+  }, 0);
+}
+
+export function subtract(...[firstArg, ...restArgs]: Args): number {
+  return restArgs.reduce((previousValue, value) => {
+    return previousValue - value;
+  }, firstArg);
+}
+
+export function multiply(...[firstArg, ...restArgs]: Args): number {
+  return restArgs.reduce((previousValue, value) => {
+    return previousValue * value;
+  }, firstArg);
+}
