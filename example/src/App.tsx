b476d04383943b275801171c8fa5a53e1f4141c1
Weslley da Silva Gomes <weslleydsg21@gmail.com>
2023-02-06 13:28:05 -0300

    feat: create equals operator

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
-import { add, divide, multiply, subtract } from 'react-native-example-package';
+import {
+  add,
+  divide,
+  equals,
+  multiply,
+  subtract,
+} from 'react-native-example-package';
 
 export default function App() {
   const [addResult, setAddResult] = React.useState<number | undefined>();
@@ -12,12 +18,14 @@ export default function App() {
     number | undefined
   >();
   const [divideResult, setDivideResult] = React.useState<number | undefined>();
+  const [equalsResult, setEqualsResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
     add({ a: 3, b: 7 }).then(setAddResult);
     subtract({ a: 3, b: 7 }).then(setSubtractResult);
     multiply({ a: 3, b: 7 }).then(setMultiplyResult);
     divide({ a: 10, b: 2 }).then(setDivideResult);
+    equals({ a: 2, b: 2 }).then(setEqualsResult);
   }, []);
 
   return (
@@ -26,6 +34,7 @@ export default function App() {
       <Text>Subtract Result: {subtractResult}</Text>
       <Text>Multiply Result: {multiplyResult}</Text>
       <Text>Divide Result: {divideResult}</Text>
+      <Text>Equals Result: {String(equalsResult)}</Text>
     </View>
   );
 }
